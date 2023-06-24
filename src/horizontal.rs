0b26f5b15e967647ed2f73b68c2e14593639e6d1
kor0 <ryusama6996@gmail.com>
2023-06-24 18:21:22 +0530

    fix(src):creation of modules #1

---
+++ src/horizontal.rs
@@ -0,0 +1,9 @@
+pub mod horizontal_module {
+    pub fn hello() {
+        println!("Hello from my_module!");
+    }
+
+    pub struct MyStruct {
+        pub value: i32,
+    }
+}
---
+++ src/horizontal_module/mod.rs
@@ -0,0 +1,8 @@
+pub mod horizontal_module {
+    pub fn hello() {
+        println!("Hello from my_module!");
+    }
+    pub struct MyStruct {
+        pub value: i32,
+    }
+}
